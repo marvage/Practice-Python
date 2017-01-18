b0550631bb7659c5a24f373ff2e5041e07127ab5
Janet Martin <marvage@users.noreply.github.com>
2017-01-18 01:17:23 -0800

    Problem 5 from Practice Python
    Updated with a more simplified solution.

--- problem5.py
+++ problem5.py
@@ -6,6 +6,5 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-print "The following numbers appear in boths lists:"
-print list(set(a).intersection(set(b)))
-    
+#same_value = set(a) & set(b)
+print same_value
